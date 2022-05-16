55e0748f99c61bbfaf3ab7d31c1014feb2e1da68
Kairat <voody2506@gmail.com>
2022-05-16 23:14:39 +0600

    Fixes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { wasmBrowserInstantiate } from "./instantiateWasm.js";
+import { wasmBrowserInstantiate } from "./iiinstantiateWasm.js";
 
 const go = new Go(); // Defined in wasm_exec.js. Don't forget to add this in your index.html.
 
