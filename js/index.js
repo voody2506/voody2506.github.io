1c2b8177a2fe8389f7ae105527f135b18e236432
Kairat <voody2506@gmail.com>
2022-05-17 01:56:23 +0600

    no message

--- js/index.js
+++ js/index.js
@@ -2,21 +2,25 @@ import { wasmBrowserInstantiate } from "/js/instantiateWasm.js";
 
 const go = new Go(); // Defined in wasm_exec.js. Don't forget to add this in your index.html.
 const WASM_URL = '/application/wasm.wasm';
+var wasm;
 
 const runWasmAdd = async () => {
-  // Get the importObject from the go instance.
-  const importObject = go.importObject;
-
-  // Instantiate our wasm module
-  const wasmModule = await wasmBrowserInstantiate(WASM_URL, importObject);
-
-  // Allow the wasm_exec go instance, bootstrap and execute our wasm module
-  go.run(wasmModule.instance);
-
-  // Call the Add function export from wasm, save the result
-  const addResult = wasmModule.instance.exports.add(24, 24);
-  // Set the result onto the body
-  document.body.textContent = `Hello World! addResult: ${addResult}`;
+  if ('instantiateStreaming' in WebAssembly) {
+    WebAssembly.instantiateStreaming(fetch(WASM_URL), go.importObject).then(function (obj) {
+      wasm = obj.instance;
+      go.run(wasm);
+      console.log('multiplied add numbers:', wasm.exports.add(5, 3));
+    })
+  } else {
+    fetch(WASM_URL).then(resp =>
+        resp.arrayBuffer()
+    ).then(bytes =>
+        WebAssembly.instantiate(bytes, go.importObject).then(function (obj) {
+          wasm = obj.instance;
+          go.run(wasm);
+        })
+    )
+  }
 };
 
 runWasmAdd();
