33cdfaf2d39037f09e22e4d330c003fd0bc7243c
Kairat <voody2506@gmail.com>
2022-05-17 03:36:32 +0600

    Update

--- js/index.js
+++ js/index.js
@@ -2,22 +2,18 @@ const go = new Go(); // Defined in wasm_exec.js. Don't forget to add this in you
 const WASM_URL = '/application/wasm.wasm';
 var wasm;
 
-function postInstantiate(obj) {
-  wasm = obj.instance;
-  go.run(wasm);
-
-  // Calling a function exported from Go to JS
-  console.log('using Go-defined function to multiply two numbers:', wasm.exports.multiply(5, 3));
-}
-
 if ('instantiateStreaming' in WebAssembly) {
-  WebAssembly.instantiateStreaming(fetch(WASM_URL), go.importObject).then(postInstantiate);
+	WebAssembly.instantiateStreaming(fetch(WASM_URL), go.importObject).then(function (obj) {
+		wasm = obj.instance;
+		go.run(wasm);
+	})
 } else {
-  fetch(WASM_URL).then(resp =>
-      resp.arrayBuffer()
-  ).then(bytes =>
-      WebAssembly.instantiate(bytes, go.importObject).then(postInstantiate)
-  )
+	fetch(WASM_URL).then(resp =>
+		resp.arrayBuffer()
+	).then(bytes =>
+		WebAssembly.instantiate(bytes, go.importObject).then(function (obj) {
+			wasm = obj.instance;
+			go.run(wasm);
+		})
+	)
 }
-
-postInstantiate();
